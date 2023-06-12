718695ea006986c8dd9040b2b192937557654cf5
Shrutika Jha <116791993+shrutika2603@users.noreply.github.com>
2023-06-12 06:38:11 +0000

    ghfj

---
+++ ch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<<"Enter any character : "<<endl;
+    cin>> c;
+    int a = c;
+    if (a>96 && a<123){
+        cout<<"This is lowercase character"<<endl;
+    }
+    else if(a>64 && a<91){
+        cout<<"This is uppercase character"<<endl;
+    }
+    else if (a>47 && a<58){
+        cout<<"This is a numeric character"<<endl;
+    }
+    else{
+        cout<<"This is a special character"<<endl;
+    }
+
+}
