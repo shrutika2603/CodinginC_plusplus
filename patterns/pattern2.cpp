be870bf5ff5e07e7e7fc5bf7f132da9821cd91a4
Shrutika Jha <shrutikajha2603@gmail.com>
2023-06-13 16:14:20 +0000

    quessssssss

--- patterns/pattern2.cpp
+++ patterns/pattern2.cpp
@@ -1,4 +1,9 @@
-/*upper triangle*/
+/*upper triangle
+1
+2 2
+3 3 3
+4 4 4 4
+*/
 #include <iostream>
 using namespace std;
 
--- patterns/pattern4.cpp
+++ patterns/pattern4.cpp
@@ -1,3 +1,9 @@
+/*
+4 4 4 4
+3 3 3 3
+2 2 2 2
+1 1 1 1
+*/
 #include <iostream>
 using namespace std;
 
@@ -6,7 +12,7 @@ int main(){
     cin>>n;
     for(i=n; i>=1; i--){
         for(j=n; j>=1; j--){
-            cout<<j<<" ";
+            cout<<i<<" ";
         }
         cout<<endl;
     }
